b2466da371bb4611b58a2202e96c0fc3bdc3a472
lokajis <alexandros.economide@gmail.com>
2024-01-04 16:42:33 +0100

    added navbar and basic root functionality

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -2,24 +2,26 @@ import React, { useState } from "react";
 import Login from "../pages/LoginPage";
 import {BrowserRouter, Routes , Route, Navigate} from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import Navbar from "./Navbar";
 
 function App() {
-  const user = false ;
+  const user = true ;
 
 
   return (
  <BrowserRouter>
+ <Navbar user={user}/>
    <Routes>
    <Route path="/" element={<HomePage />} />
 
     <Route
      path="/login"
-      element= {user ? <Navigate to="/" />: <Login/> } />
+      element= {user ? <Navigate to="/" />: <Login/> } /> 
 
   {/* <Route path="/Nposts/:id"
       element= {user ? <nPosts>: <Navigate to = "/login"> } /> )(not done yet) */}
 
-</Routes>
+ </Routes> 
 </BrowserRouter>
   );
 
